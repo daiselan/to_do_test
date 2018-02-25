ed833611e9823a2fe402a69df169528a464b9fb5
daiselan <daiselan@gmail.com>
2018-02-25 13:10:35 -0800

    minor change

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
-var request = app.get('/', function(req, res) {
+
+var todos = [
+	{
+		id: 1,
+		desc: 'first one'
+	},
+	{
+		id: 2,
+		desc: 'second one'
+	}
+]
+var request1 = app.get('/', function(req, res) {
 	res.send('toDo API root');
+});
+
+var request2 = app.get('/todos', function(req, res) {
+	res.json(todos);
 });
 
 app.listen(PORT, function(){
